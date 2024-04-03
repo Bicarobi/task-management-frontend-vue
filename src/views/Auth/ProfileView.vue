ac0887c3dac6683049bd317371d777d9a5b7cf6f
Robert C <cmrecki.robert@gmail.com>
2024-04-03 23:38:36 +0200

    Added file size limit of 3MB

--- src/views/Auth/ProfileView.vue
+++ src/views/Auth/ProfileView.vue
@@ -11,6 +11,7 @@
 				<label for="imageInput" class="custom-file-upload"><UploadIcon /> {{ $t("profile-view.upload") }} </label>
 				<input type="file" id="imageInput" accept="image/*" @change="handleSubmit" />
 			</form>
+			<div class="error-text" v-show="this.showFileError">{{ $t("profile-view.file-error") }}</div>
 			<div class="username-container">
 				{{ $t("user-auth.username") }}:
 				<span class="username">{{ this.$myGlobalVariable.username }}</span>
@@ -27,34 +28,48 @@ import UploadIcon from "../../components/icons/UploadIcon.vue";
 export default {
 	name: "ProfileView",
 	components: { UploadIcon },
+	data() {
+		return {
+			showFileError: false,
+		};
+	},
 	methods: {
 		async handleSubmit() {
 			var input = document.getElementById("imageInput");
 
 			if (input.files && input.files[0]) {
 				var file = input.files[0];
-				var formData = new FormData();
-				formData.append("image", file);
+				const fileSize = file.size;
+				const fileMb = fileSize / 1024 ** 2;
+
+				if (fileMb <= 3) {
+					this.showFileError = false;
+
+					var formData = new FormData();
+					formData.append("image", file);
+
+					const requestOptions = {
+						method: "POST",
+						headers: { Authorization: "Bearer " + this.$myGlobalVariable.accessToken },
+						body: formData,
+					};
+
+					this.$emit("show-loading", true);
+
+					await fetch(process.env.VUE_APP_BASE_URL + "/files", requestOptions)
+						.then((res) => res.text())
+						.then((data) => {
+							this.$myGlobalVariable.profileImage = data;
+							localStorage.profileImage = this.$myGlobalVariable.profileImage;
+							console.log(this.$myGlobalVariable.profileImage);
+						});
+
+					this.$emit("close-create-task-modal");
+					this.$emit("show-loading", false);
+				} else {
+					this.showFileError = true;
+				}
 			}
-
-			const requestOptions = {
-				method: "POST",
-				headers: { Authorization: "Bearer " + this.$myGlobalVariable.accessToken },
-				body: formData,
-			};
-
-			this.$emit("show-loading", true);
-
-			await fetch(process.env.VUE_APP_BASE_URL + "/files", requestOptions)
-				.then((res) => res.text())
-				.then((data) => {
-					this.$myGlobalVariable.profileImage = data;
-					localStorage.profileImage = this.$myGlobalVariable.profileImage;
-					console.log(this.$myGlobalVariable.profileImage);
-				});
-
-			this.$emit("close-create-task-modal");
-			this.$emit("show-loading", false);
 		},
 	},
 };
