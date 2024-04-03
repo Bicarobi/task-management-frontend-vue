2c49ebd742d9a375f92b462efb136628e4beb464
Robert C <cmrecki.robert@gmail.com>
2024-04-03 21:37:33 +0200

    Stylied profile view page, added upload image, changed navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -21,24 +21,24 @@
 						<!-- <router-link :to="{ name: 'createTask' }">Create a Task</router-link>
 					<router-link :to="{ name: 'deleteTask' }">Delete a Task</router-link> -->
 
-						<img
-							class="profile-image"
-							:src="this.$myGlobalVariable.profileImage ? this.$myGlobalVariable.profileImage : require('../assets/placeholder.png')"
-							v-if="this.$myGlobalVariable.username"
-						/>
+						<router-link :to="{ name: 'profile', params: { username: this.$myGlobalVariable.username } }" v-if="this.$myGlobalVariable.username" @click="openNavbarMenu(false, false)">
+							<div class="profile-image-router">
+								{{ this.$myGlobalVariable.username }}
+								<img class="profile-image" :src="this.$myGlobalVariable.profileImage ? this.$myGlobalVariable.profileImage : require('../assets/placeholder.png')" /></div
+						></router-link>
 
-						<router-link
+						<!-- <router-link
 							class="username"
 							:to="{ name: 'profile', params: { username: this.$myGlobalVariable.username } }"
 							v-if="this.$myGlobalVariable.username"
 							@click="openNavbarMenu(false, false)"
 							>{{ this.$myGlobalVariable.username }}</router-link
-						>
+						> -->
 
 						<!-- <SettingsIcon @click="openSettings(false, false)" v-if="this.$myGlobalVariable.username" />
 
 				<SettingsMenu v-if="this.$myGlobalVariable.username && this.settingsMenuOpened" @close-menu="openSettings(true, false)" /> -->
-						<button @click="this.$emit('log-out')" v-if="this.$myGlobalVariable.username">{{ $t("user-auth.sign-out") }}</button>
+						<!-- <button @click="this.$emit('log-out')" v-if="this.$myGlobalVariable.username">{{ $t("user-auth.sign-out") }}</button> -->
 
 						<router-link :to="{ name: 'signIn' }" v-if="!this.$myGlobalVariable.username" @click="openNavbarMenu(false, false)">{{ $t("user-auth.sign-in") }}</router-link>
 
--- src/views/Auth/ProfileView.vue
+++ src/views/Auth/ProfileView.vue
@@ -1,1 +1,61 @@
-<template></template>
+<template>
+	<div class="profile-container">
+		<div class="profile">
+			<img
+				class="profile-image"
+				:src="this.$myGlobalVariable.profileImage ? this.$myGlobalVariable.profileImage : require('../../assets/placeholder.png')"
+				v-if="this.$myGlobalVariable.username"
+			/>
+
+			<form @submit.prevent="handleSubmit">
+				<label for="imageInput" class="custom-file-upload"><UploadIcon /> {{ $t("profile-view.upload") }} </label>
+				<input type="file" id="imageInput" accept="image/*" @change="handleSubmit" />
+			</form>
+			<div class="username-container">
+				{{ $t("user-auth.username") }}:
+				<span class="username">{{ this.$myGlobalVariable.username }}</span>
+			</div>
+
+			<button @click="this.$emit('log-out')" v-if="this.$myGlobalVariable.username">{{ $t("user-auth.sign-out") }}</button>
+		</div>
+	</div>
+</template>
+
+<script>
+import UploadIcon from "../../components/icons/UploadIcon.vue";
+
+export default {
+	name: "ProfileView",
+	components: { UploadIcon },
+	methods: {
+		async handleSubmit() {
+			var input = document.getElementById("imageInput");
+
+			if (input.files && input.files[0]) {
+				var file = input.files[0];
+				var formData = new FormData();
+				formData.append("image", file);
+			}
+
+			const requestOptions = {
+				method: "POST",
+				headers: { Authorization: "Bearer " + this.$myGlobalVariable.accessToken },
+				body: formData,
+			};
+
+			this.$emit("show-loading", true);
+
+			await fetch(process.env.VUE_APP_BASE_URL + "/files", requestOptions)
+				.then((res) => res.text())
+				.then((data) => {
+					this.$myGlobalVariable.profileImage = data;
+					localStorage.profileImage = this.$myGlobalVariable.profileImage;
+					console.log(this.$myGlobalVariable.profileImage);
+				});
+
+			this.$emit("close-create-task-modal");
+			this.$emit("show-loading", false);
+		},
+	},
+};
+</script>
